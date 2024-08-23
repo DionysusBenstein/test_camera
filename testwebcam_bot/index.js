d27a21b5d22830e08a13b5cd9708feb15c8766a7
DionysusBenstein <dionysusbenstein@gmail.com>
2024-08-23 20:51:40 +0300

    update

--- testwebcam_bot/index.js
+++ testwebcam_bot/index.js
@@ -3,7 +3,7 @@ const { Telegraf, Markup } = require('telegraf')
 const bot = new Telegraf('7369645200:AAEWnnCuBXMu6FGQzFKAjSyQYak6P4gm0nk')
 bot.command('oldschool', (ctx) => {
   const reply_markup  = Markup.inlineKeyboard([[
-    Markup.button.webApp('Open app', 'https://google.com'),
+    Markup.button.webApp('Open app', 'https://dionysusbenstein.github.io/test_camera'),
   ]]);
 
   ctx.reply('Start mine',  reply_markup );
