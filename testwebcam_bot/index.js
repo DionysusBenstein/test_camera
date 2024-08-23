be2867fdc6675570403cf666fbf6264efea14728
DionysusBenstein <dionysusbenstein@gmail.com>
2024-08-23 20:42:21 +0300

    first commit

---
+++ testwebcam_bot/index.js
@@ -0,0 +1,18 @@
+const { Telegraf, Markup } = require('telegraf')
+
+const bot = new Telegraf('7369645200:AAEWnnCuBXMu6FGQzFKAjSyQYak6P4gm0nk')
+bot.command('oldschool', (ctx) => {
+  const reply_markup  = Markup.inlineKeyboard([[
+    Markup.button.webApp('Open app', 'https://google.com'),
+  ]]);
+
+  ctx.reply('Start mine',  reply_markup );
+})
+
+
+bot.command('hipster', Telegraf.reply('λ'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
